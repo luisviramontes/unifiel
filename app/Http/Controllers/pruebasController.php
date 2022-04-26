6a6fd5c0ee163b59a13a179617bb05d88a7b68d5
luisviramontes <luis_alfonso133@hotmail.com>
2022-04-26 13:37:16 -0500

    Cambios

--- app/Http/Controllers/pruebasController.php
+++ app/Http/Controllers/pruebasController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use ZipArchive;
 use App\autoridadesCertModel;
+use DB;
 
 class pruebasController extends Controller
 {
